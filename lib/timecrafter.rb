f42db4dbaa8d397a0a1239b0daba61710a50cd79
Banura Randika <randika.banura@gmail.com>
2020-02-10 16:22:39 +0530

    Issues fixed

--- lib/timecrafter.rb
+++ lib/timecrafter.rb
@@ -12,27 +12,48 @@ module Timecrafter
 
     hours = hours.to_f
     hour = hours.to_i
-    precision = _options[:precision] || 2
-    
-    minutes_fraction = hours.modulo(1).round(precision)
 
-    minutes = (60 * minutes_fraction).to_i
-    create_default_hourly(hour, minutes)
+    param_precision = _options[:precision] || 2
+    param_seconds = _options[:seconds] || false
+    param_theme = _options[:theme] || 'long'
+
+    minutes_fraction = hours.modulo(1).round(param_precision)
+
+    theme = param_theme
+    seconds = nil
+    minutes = if !param_seconds
+                get_fraction(60, minutes_fraction)
+              else
+                get_fraction(60, minutes_fraction, param_precision)
+              end
+
+    if param_seconds
+      seconds_fraction = minutes.modulo(1).round(param_precision)
+      seconds = get_fraction(60, seconds_fraction)
+      minutes = minutes.floor
+    end
+
+    create_default_hourly(hour, minutes, seconds, theme)
   end
 
 
   private
 
-  def self.create_default_hourly(hour, minutes)
+  def self.create_default_hourly(hour, minutes, seconds = nil, theme = 'long')
     hourly_humanize = ''
 
     if more_than_zero?(hour)
-      hourly_humanize += "#{hour} hour#{'s' if more_than_one?(hour)}"
+      hourly_humanize += "#{hour} hour#{'s' if validate_theme_number(hour, theme)}"
     end
 
-    if more_than_zero?(minutes)
-      hourly_humanize += " #{minutes} minute#{'s' if more_than_one?(minutes)}"
+    if more_than_zero?(minutes) || seconds&.positive?
+      hourly_humanize += " #{minutes} minute#{'s' if validate_theme_number(minutes, theme)}"
     end
+
+    if seconds&.positive?
+      hourly_humanize += " #{seconds} second#{'s' if validate_theme_number(seconds, theme)}"
+    end
+
     hourly_humanize.strip
   end
 
@@ -42,6 +63,14 @@ module Timecrafter
 
   def self.more_than_one?(number)
     number > 1
+  end
+
+  def self.get_fraction(multiplier, fraction, precision = 0)
+    (multiplier * fraction).round(precision)
+  end
+  
+  def self.validate_theme_number(number, theme)
+    more_than_one?(number) && theme == 'long'
   end
 
   def self.error_handling(key, hour)
--- lib/timecrafter/options_handling.rb
+++ lib/timecrafter/options_handling.rb
@@ -9,8 +9,12 @@ module Timecrafter
         case k
         when :precision
           error_handling_number(k, v)
+        when :seconds
+          error_handling_boolean(k, v)
+        when :theme
+          error_handling_theme(v)
         else
-        
+          not_available_argument(k, v)
         end
       end
     end
@@ -21,10 +25,19 @@ module Timecrafter
     def self.error_handling_number(key, number)
       raise ArgumentError, "#{key.to_s.titleize} argument is not numeric." unless number.is_a? Numeric
       raise ArgumentError, "#{key.to_s.titleize} argument is negative." if number.negative?
+      raise ArgumentError, "#{key.to_s.titleize} argument cannot be less than 2." if number < 2
     end
-    
-    def not_available_argument(key, number)
+
+    def self.error_handling_boolean(key, boolean)
+      raise ArgumentError, "#{key.to_s.titleize} argument is not boolean." unless [true, false].include? boolean
+    end
+
+    def self.not_available_argument(key, number)
       raise ArgumentError, "#{key.to_s.titleize} argument is not a available argument."
+    end
+
+    def self.error_handling_theme(theme)
+      raise ArgumentError, 'Theme argument is not valid.' unless ['short', 'long'].include? theme
     end
   end
 end
